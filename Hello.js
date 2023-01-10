070f6b12af5c847b92dc56cd229a8ac5925215b7
Supraja Ayanavolu <82303022+supraja2002-hema@users.noreply.github.com>
2023-01-10 13:09:51 +0530

    This is the file for challenge "Hello, World!" on Day-1

---
+++ Hello.js
@@ -0,0 +1,8 @@
+function hello()
+{
+    console.log("Hello, World!");
+    console.log("This is Supraja Ayanavolu, I'm attending GHW January");
+    console.log("I have recently started learning JS, so I thought of completing this task using JS");
+}
+
+hello();
